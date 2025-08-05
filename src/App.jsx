45cdab1226cef1c3fd216d72ef8c8e1cbeb3b380
joshua <iuttn123@gmail.com>
2025-08-05 21:44:46 +0900

    08/05

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Circle from "./Circle";
+
 function App() {
-  return <>hello react!</>;
+  return (
+    <>
+      <Circle size="50px" bg="red"></Circle>
+      <Circle size="100px" bg="blue"></Circle>
+      <Circle size="150px" bg="green"></Circle>
+    </>
+  );
 }
 
 export default App;
